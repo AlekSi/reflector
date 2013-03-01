eb6fe4063b33df83fbdb1e73e0debce2703e3c99
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2013-03-01 18:05:53 +0400

    Replace Latin with English.

--- reflector.go
+++ reflector.go
@@ -8,9 +8,9 @@ import (
 )
 
 // Converts value to kind. Panics if it can't be done.
-type Convertor func(value interface{}, kind reflect.Kind) interface{}
+type Converter func(value interface{}, kind reflect.Kind) interface{}
 
-// Requires value to be exactly of specified kind.
+// Converter: requires value to be exactly of specified kind.
 func NoConvert(value interface{}, kind reflect.Kind) interface{} {
 	switch kind {
 	case reflect.Bool:
@@ -52,7 +52,7 @@ func NoConvert(value interface{}, kind reflect.Kind) interface{} {
 	panic(fmt.Errorf("Can't convert %#v to %s", value, kind))
 }
 
-// Converts value to kind using strconv.Parse* functions.
+// Converter: uses strconv.Parse* functions.
 func Strconv(value interface{}, kind reflect.Kind) (res interface{}) {
 	e := fmt.Errorf("can't convert %#v to %s", value, kind)
 	s := fmt.Sprint(value)
@@ -93,12 +93,12 @@ func Strconv(value interface{}, kind reflect.Kind) (res interface{}) {
 	panic(e)
 }
 
-// Converts a map to struct using convertor function.
+// Converts a map to struct using converter function.
 // Only exported struct fields are set. Omitted or extra values in map are ignored.
 // Tag may be used to change mapping between struct field and map key.
 // Currently supports bool, ints, uints, floats, strings.
 // Panics in case of error.
-func MapToStruct(m map[string]interface{}, structPointer interface{}, convertor Convertor, tag string) {
+func MapToStruct(m map[string]interface{}, structPointer interface{}, converter Converter, tag string) {
 	structPointerType := reflect.TypeOf(structPointer)
 	if structPointerType.Kind() != reflect.Ptr {
 		panic(fmt.Errorf("Expected pointer to struct as second argument, got %s", structPointerType.Kind()))
@@ -141,19 +141,19 @@ func MapToStruct(m map[string]interface{}, structPointer interface{}, convertor 
 		kind := f.Kind()
 		switch kind {
 		case reflect.Bool:
-			f.SetBool(convertor(v, kind).(bool))
+			f.SetBool(converter(v, kind).(bool))
 
 		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-			f.SetInt(convertor(v, kind).(int64))
+			f.SetInt(converter(v, kind).(int64))
 
 		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-			f.SetUint(convertor(v, kind).(uint64))
+			f.SetUint(converter(v, kind).(uint64))
 
 		case reflect.Float32, reflect.Float64:
-			f.SetFloat(convertor(v, kind).(float64))
+			f.SetFloat(converter(v, kind).(float64))
 
 		case reflect.String:
-			f.SetString(convertor(v, kind).(string))
+			f.SetString(converter(v, kind).(string))
 
 		default:
 			// not implemented
@@ -163,7 +163,7 @@ func MapToStruct(m map[string]interface{}, structPointer interface{}, convertor 
 }
 
 // Converts a slice of maps to a slice of structs. Uses MapToStruct().
-func MapsToStructs(s []map[string]interface{}, slicePointer interface{}, convertor Convertor, tag string) {
+func MapsToStructs(s []map[string]interface{}, slicePointer interface{}, converter Converter, tag string) {
 	slicePointerType := reflect.TypeOf(slicePointer)
 	if slicePointerType.Kind() != reflect.Ptr {
 		panic(fmt.Errorf("Expected pointer to slice of structs as second argument, got %s", slicePointerType.Kind()))
@@ -182,17 +182,17 @@ func MapsToStructs(s []map[string]interface{}, slicePointer interface{}, convert
 	slice := reflect.MakeSlice(sliceType, 0, len(s))
 	for _, m := range s {
 		str := reflect.New(structType)
-		MapToStruct(m, str.Interface(), convertor, tag)
+		MapToStruct(m, str.Interface(), converter, tag)
 		slice = reflect.Append(slice, str.Elem())
 	}
 	reflect.ValueOf(slicePointer).Elem().Set(slice)
 }
 
 // Variant of MapsToStructs() with relaxed signature.
-func MapsToStructs2(s []interface{}, slicePointer interface{}, convertor Convertor, tag string) {
+func MapsToStructs2(s []interface{}, slicePointer interface{}, converter Converter, tag string) {
 	m := make([]map[string]interface{}, len(s))
 	for index, i := range s {
 		m[index] = i.(map[string]interface{})
 	}
-	MapsToStructs(m, slicePointer, convertor, tag)
+	MapsToStructs(m, slicePointer, converter, tag)
 }
